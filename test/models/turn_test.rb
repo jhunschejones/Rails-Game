2d8932688bcbf025c849d19b05720ef49edab8d1
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-05-23 20:27:14 -0700

    Add turn tests to confirm on cascade delete works

--- test/models/turn_test.rb
+++ test/models/turn_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
+# bundle exec ruby -Itest test/models/turn_test.rb
 class TurnTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe "when deleting a turn" do
+    test "related SelectedOption records are cleaned up" do
+      assert turns(:one).selected_options.size > 0, "there are no selected_option fixtures to test"
+      assert_difference 'SelectedOption.count', -turns(:one).selected_options.size do
+        turns(:one).destroy
+      end
+    end
+
+    test "Option records are not affected" do
+      assert games(:one).options.size > 0, "there are no option fixtures to test"
+      assert_no_difference 'Option.count' do
+        turns(:one).destroy
+      end
+    end
+  end
 end
